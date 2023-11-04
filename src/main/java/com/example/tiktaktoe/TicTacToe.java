d501886831cc7d2cd19003bc3105666387dcc182
Dimas <dimaszaragoza16@gmail.com>
2023-11-04 16:46:05 +0100

    I made the code have one method what make the player win in vertical or horizontal

--- src/main/java/com/example/tiktaktoe/TicTacToe.java
+++ src/main/java/com/example/tiktaktoe/TicTacToe.java
@@ -3,7 +3,6 @@ package com.example.tiktaktoe;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
@@ -14,37 +13,43 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Random;
 
-public class HelloApplication extends Application {
+public class TicTacToe extends Application {
 
     static int turnoJuagador = turnoJugarAleatorio();
-
+    static GridPane tableroVisual = new GridPane();
+    static String[][] tablero = {{"", "", ""}, {"", "", ""}, {"", "", ""}};
 
     @Override
     public void start(Stage stage) throws IOException {
-        GridPane tablero = new GridPane();
 
         for (int i = 0; i < 3; i++){
             for (int j = 0; j < 3; j++){
                 Button newButton = new Button();
                 newButton.setPrefSize(100, 100);
                 newButton.setFont(new Font(20));
+                int finalI = i;
+                int finalJ = j;
                 newButton.setOnAction(new EventHandler<ActionEvent>() {
                     @Override
                     public void handle(ActionEvent actionEvent) {
                         if(turnoJuagador == 0){
+                            tablero[finalI][finalJ] = "O";
                             newButton.setText("O");
+                            Ganar("O");
                         }else{
+                            tablero[finalI][finalJ] = "X";
                             newButton.setText("X");
+                            Ganar("X");
                         }
                         siguienteJugador();
                         newButton.setDisable(true);
                     }
                 });
-                tablero.add(newButton, i, j);
+                tableroVisual.add(newButton, i, j);
             }
         }
         VBox vBox = new VBox();
-        vBox.getChildren().addAll(tablero);
+        vBox.getChildren().addAll(tableroVisual);
         Scene scene = new Scene(vBox,200,200);
         stage.setScene(scene);
         stage.show();
@@ -59,6 +64,29 @@ public class HelloApplication extends Application {
             turnoJuagador = 1;
         }else{
             turnoJuagador = 0;
+        }
+    }
+
+    private void Ganar(String jugador){
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+
+                if(tablero[i][0].equals(jugador)  && tablero[i][1].equals(jugador) && tablero[i][2].equals(jugador)){
+                    if(jugador.equals("X")){
+                        System.out.println("Ganador Player 2");
+                    }else{
+                        System.out.println("Ganador Player 1");
+                    }
+                }
+                if(tablero[0][j].equals(jugador)  && tablero[1][j].equals(jugador) && tablero[2][j].equals(jugador)){
+                    System.out.println("Ganador Player 2");
+                    if(jugador.equals("X")){
+                        System.out.println("Ganador Player 2");
+                    }else{
+                        System.out.println("Ganador Player 1");
+                    }
+                }
+            }
         }
     }
 
